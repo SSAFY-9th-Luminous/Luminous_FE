fd2de849e41be1a5c89ec104b9a82df9950bc003
Chae <eqholic1125@gmail.com>
2023-05-24 16:58:03 +0900

    feat: 캠핑장 리스트에 위치기반으로 목록 요청 기능 API 구현

--- luminous/src/components/camping/CampingView.vue
+++ luminous/src/components/camping/CampingView.vue
@@ -2,26 +2,80 @@
   <b-container class="bv-example-row mt-3">
 
     <div id="br" class="mb-5"></div>
+    <!-- title & locationInfo -->
+    <div id="top-container">
+      <div id="top-left">
+        <div id="title-area">
+          <h3 v-if="camping.campingName" id="title" class="text-left mb-3 mr-3">{{camping.campingName}}</h3>
+          <h3 v-else class="text-left">캠핑장</h3>
 
-    <!-- title -->
-    <b-row>
-      <b-col>
-        <h3 v-if="camping.campingName" class="text-left mb-3">{{camping.campingName}}</h3>
-        <h3 v-else class="text-left mb-3">캠핑장</h3>
-      </b-col>
-    </b-row>
+          <p v-if="camping.lineIntro" class="text-left">{{camping.lineIntro}}</p>
+          <p v-else class="text-left">맑은 공기 속에서 밤하늘의 별을 보며 잠들 수 있는 캠핑장</p>
+        </div>
+
+        <div id="location-area" class="text-left mb-3">
+
+          <img class="image" :src= "require('@/assets/img/camping/location.png')" >
+          <a class="link" :href="`https://www.google.com/maps/place/${camping.address.replace(' ', '+')}`" target="_blank">{{camping.address}}</a>
+        </div>
+        
+      </div>
+      <div id="top-right">
+        <div id="rate-area">
+          <div class="rate-show-area">
+            <div class="star-ratings">
+              <div 
+                id="stars"
+                class="star-ratings-fill space-x-2 text-lg"
+                :style="{ width: ratingToPercent + '%' }"
+              >
+                <span>★&nbsp;</span><span>★&nbsp;</span><span>★&nbsp;</span><span>★&nbsp;</span><span>★</span>
+              </div>
+              <div class="star-ratings-base space-x-2 text-lg">
+                <span>★&nbsp;</span><span>★&nbsp;</span><span>★&nbsp;</span><span>★&nbsp;</span><span>★</span>
+              </div>
+            </div>
+          </div>
+
+          <div id="rate-stars-container" class="mt-3">
+            <div id="rate" :style="{ display: showRate ? 'block' : 'none' }">
+              <div class="star-rating space-x-4 mx-auto">
+                <input type="radio" id="5-stars" name="rating" value="5" v-model="ratings"/>
+                <label for="5-stars" class="star pr-4">★</label>
+                <input type="radio" id="4-stars" name="rating" value="4" v-model="ratings"/>
+                <label for="4-stars" class="star">★</label>
+                <input type="radio" id="3-stars" name="rating" value="3" v-model="ratings"/>
+                <label for="3-stars" class="star">★</label>
+                <input type="radio" id="2-stars" name="rating" value="2" v-model="ratings"/>
+                <label for="2-stars" class="star">★</label>
+                <input type="radio" id="1-star" name="rating" value="1" v-model="ratings" />
+                <label for="1-star" class="star">★</label>
+              </div>
+            </div>
+            <div id="rate-button" class="rate-button-area mt-1" :style="{ display: showButton ? 'none' : 'block' }">
+              <button class="rate-button" @click="rate">평점 주기</button>
+            </div>
+            <div id="submit-rate" class="rate-button-area mt-1" :style="{ display: showRate ? 'block' : 'none' }">
+              <button class="rate-button button-color" @click="submitRate">평점 제출</button>
+            </div>
+          </div>
+          
+
+        </div>
+      </div>
+    </div>
 
     <!-- intro -->
     <b-row>
       <b-col>
-        <p v-if="camping.lineIntro" class="text-left">{{camping.lineIntro}}</p>
-        <p v-else class="text-left">맑은 공기 속에서 밤하늘의 별을 보며 잠들 수 있는 캠핑장</p>
+        
       </b-col>
     </b-row>
     
-    <div class=" md-5">
+    <div id="slider-container">
       <vueper-slides id="slider">
         <vueper-slide
+          class="vueper-slide"
           v-for="(image, i) in images"
           :key="i"
           :image="image.imageUri">
@@ -44,7 +98,7 @@
 
           <div class="content-detail">
             <div class="content-sub-title"><h6>홈페이지</h6></div>
-            <div v-if="camping.homepage" class="content col-lg-10">{{camping.homepage}}</div>
+            <div v-if="camping.homepage" class="content col-lg-10"><a class="link" :href="camping.homepage" target="_blank">{{camping.homepage}}</a></div>
             <div v-else class="content col-lg-10"> - </div>
           </div>
 
@@ -54,11 +108,6 @@
             <div v-else class="content col-lg-10"> - </div>
           </div>
           
-          <div class="content-detail">
-            <div class="content-sub-title"><h6>상세 설명</h6></div>
-            <div v-if="camping.intro" class="content col-lg-10">{{camping.intro}}</div>
-            <div v-else class="content col-lg-10"> - </div>
-          </div>
 
         </div>
 
@@ -73,17 +122,28 @@
         <div class="content-left-area col-lg-9">
 
           <div class="content-detail">
-            <div class="content-sub-title"><h6>글램핑 정보</h6></div>
+            <div class="content-sub-title"><h6>캠핑장 테마</h6></div>
+            <div v-if="camping.theme" class="content col-lg-10">{{camping.theme}}</div>
+            <div v-else class="content col-lg-10"> - </div>
+          </div>
+
+          <div class="content-detail">
+            <div class="content-sub-title"><h6>글램핑 옵션</h6></div>
             <div v-if="camping.glamping" class="content col-lg-10">{{camping.glamping}}</div>
             <div v-else class="content col-lg-10"> - </div>
           </div>
 
           <div class="content-detail">
-            <div class="content-sub-title"><h6>카라반 정보</h6></div>
-            <div v-if="camping.caravan" calss="content col-lg-10">{{camping.caravan}}</div>
+            <div class="content-sub-title"><h6>카라반 옵션</h6></div>
+            <div v-if="camping.caravan" class="content col-lg-10">{{camping.caravan}}</div>
             <div v-else class="content col-lg-10"> - </div>
           </div>
 
+          <div class="content-detail">
+            <div class="content-sub-title"><h6>주변 편의시설</h6></div>
+            <div v-if="camping.facility" class="content col-lg-10">{{camping.facility}}</div>
+            <div v-else class="content col-lg-10"> - </div>
+          </div>
 
 
         </div>
@@ -94,6 +154,23 @@
     <div class="border"></div>
 
     <div class="content-area">
+
+      <div class="content-title-area col-lg-2">
+          <h5 class="text-left"><h5>상세 설명</h5></h5>
+        </div>
+        
+        <div class="content-left-area col-lg-9">
+          <div class="content-detail">
+            <div v-if="camping.intro" class="content text-left">{{camping.intro}}</div>
+            <div v-else class="content col-lg-10"> - </div>
+          </div>
+        </div>
+    </div>
+
+    <div class="border"></div>
+
+
+    <div class="content-area">
       <div class="content-title-area col-lg-2">
         <h5 class="text-left">지도</h5>
       </div>
@@ -101,7 +178,7 @@
       </div>
     </div>
 
-    <div class="md-5 pd-5">
+    <div class="mt-4 mb-5 pd-5">
       <div id = "map"></div>
     </div>
 
@@ -113,7 +190,7 @@
 
 <script>
 // import moment from "moment";
-import { getCamping } from "@/api/camping";
+import { getCamping, submitRate } from "@/api/camping";
 import { mapState } from "vuex";
 import { VueperSlides, VueperSlide } from 'vueperslides'
 import 'vueperslides/dist/vueperslides.css'
@@ -127,11 +204,14 @@ export default {
       slides: [
       ],
       camping:{
-
+        address:""
       },
       images: {
 
       },
+      showRate : false,
+      showButton: false,
+      ratings:0,
       map:null,
       marker:null,
       isUserid: false,
@@ -144,6 +224,10 @@ export default {
 
   computed: {
     ...mapState(memberStore, ["userInfo"]),
+    ratingToPercent() {
+      const score = +this.ratings * 20;
+      return score + 1.5;
+    },
   },
   created() {
     /* global kakao */
@@ -157,7 +241,8 @@ export default {
       ({ data }) => {
         this.camping = data.result.camping;
         this.images = data.result.campingImageList;
-        console.log(this.images)
+        this.ratings = data.result.camping.rate;
+
         const script = document.createElement("script");
         script.onload = () => kakao.maps.load(this.initMap);
         script.src = "//dapi.kakao.com/v2/maps/sdk.js?appkey=" +
@@ -175,6 +260,35 @@ export default {
   methods: {
     moveList() {
       this.$router.push({ name: "campinglist" });
+    },
+    rate() {
+      this.showRate = !this.showRate;
+      this.showButton = !this.showButton;
+    },
+    submitRate() {
+      this.rate();
+
+      let param = {
+        id: this.camping.id,
+        rate: this.ratings,
+      };
+      
+      submitRate(
+        param,
+        ({ data }) => {
+          let msg = "등록 처리시 문제가 발생했습니다.";
+
+          if (data.isSuccess === true) {
+            msg = "등록이 완료되었습니다.";
+            this.ratings = data.result.rate;
+
+          }
+          alert(msg);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
     },
     initMap() {
       var map = new kakao.maps.Map(document.getElementById('map'), {
@@ -207,11 +321,47 @@ export default {
 </script>
 
 <style>
+.image {
+  width: 25px;
+  height: 25px;
+}
+
+#top-container {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+
+#title-area {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+
+#rate-area{
+  display: flex;
+  flex-direction: column;
+}
+
+#rate {
+  display: none;
+}
+
 #content-main-container {
   margin-top: 50px;
   margin-bottom: 50px;
   display: felx;
   flex-direction: column;
+}
+
+#slider-container {
+  border-radius: 10px;
+}
+
+.vueperslides__parallax-wrapper{
+  background-color: white;
+    border-radius: 18px;
+
 }
 
 .content-area {
@@ -229,7 +379,7 @@ export default {
   display: flex;
   flex-direction: row;
   justify-content: space-between;
-  margin-bottom: 10px;
+  margin-bottom: 25px;
 }
 
 .tel {
@@ -251,6 +401,15 @@ export default {
   margin-bottom: 20px;
 }
 
+.link {
+  color: black;
+  text-decoration: underline;
+}
+
+.link:hover {
+  color: rgb(129, 129, 129);
+}
+
 #br {
   height: 1px;
   width: 100%;
@@ -259,12 +418,109 @@ export default {
 
 #map {
   width: 100%;
-  height: 300px;
+  height: 500px;
   margin-bottom: 50px;
+  border-radius: 18px;
 }
 
-#slider {
-  
+/* 별점 */
+
+#rate-stars-container {
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+}
+
+.star {
+  margin : 0px;
+}
+
+.rate-show-area {
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+}
+
+.star-ratings {
+  color: #aaa9a9; 
+  position: relative;
+  unicode-bidi: bidi-override;
+  width: max-content;
+  -webkit-text-fill-color: transparent; /* Will override color (regardless of order) */
+  -webkit-text-stroke-width: 1.3px;
+  -webkit-text-stroke-color: #2b2a29;
+}
+ 
+.star-ratings-fill {
+  color: #fff58c;
+  padding: 0;
+  position: absolute;
+  z-index: 1;
+  display: flex;
+  top: 0;
+  left: 0;
+  overflow: hidden;
+  -webkit-text-fill-color: gold;
+}
+ 
+.star-ratings-base {
+  z-index: 0;
+  padding: 0;
+}
+
+
+.star-rating {
+  display: flex;
+  flex-direction: row-reverse;
+  font-size: 1.5rem;
+  line-height: 2.5rem;
+  justify-content: space-around;
+  padding: 0 0.2em;
+  text-align: center;
+  width: 5em;
+}
+ 
+.star-rating input {
+  display: none;
+}
+ 
+.star-rating label {
+  -webkit-text-fill-color: transparent; /* Will override color (regardless of order) */
+  -webkit-text-stroke-width: 2.3px;
+  -webkit-text-stroke-color: #2b2a29;
+  cursor: pointer;
+}
+ 
+.star-rating :checked ~ label {
+  -webkit-text-fill-color: gold;
+}
+ 
+.star-rating label:hover,
+.star-rating label:hover ~ label {
+  -webkit-text-fill-color: #fff58c;
+}
+
+.rate-button {
+  height: 30px;
+  width: 85px;
+  background-color: white;
+  color: black;
+  border-radius: 5px;
+  text-decoration: none;
+}
+
+.rate-button-area {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+
+#submit-rate {
+  display: none;
+}
+
+.button-color {
+  background-color: bisque;
 }
 
 </style>
