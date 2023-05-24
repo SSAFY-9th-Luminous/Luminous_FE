3304938623d50208130abbe1bda8f72908b9b402
Chae <eqholic1125@gmail.com>
2023-05-24 19:37:37 +0900

    feat: 천문대 현재위치 기반으로 목록 불러오기 기능 구현

--- luminous/src/api/observatory.js
+++ luminous/src/api/observatory.js
@@ -7,11 +7,15 @@ async function observatoryMap(param, success, fail) {
 }
 
 async function listObservatory(param, success, fail) {
-  await api.get('/observatory', {params:param}).then(success).catch(fail);
+  await api.get("/observatory", { params: param }).then(success).catch(fail);
 }
 
 async function getObservatory(id, success, fail) {
   await api.get(`/observatory/${id}`).then(success).catch(fail);
 }
 
-export { observatoryMap, listObservatory, getObservatory };
+async function getObservatoryListByLocation(success, fail) {
+  await api.get(`/observatory/location`).then(success).catch(fail);
+}
+
+export { observatoryMap, listObservatory, getObservatory, getObservatoryListByLocation };
