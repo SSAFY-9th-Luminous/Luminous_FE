8042c1e166b241eb628826828878bfecadd92c7b
이준혁 <hol22mol22@gmail.com>
2023-05-24 13:08:44 +0900

    feat : 별자리 마우스 휠과 키보드로 이동 이벤트 리스너 처리

--- luminous/src/components/constellation/ConstellationList.vue
+++ luminous/src/components/constellation/ConstellationList.vue
@@ -1,16 +1,20 @@
 <template>
   <div class="container">
-    <carousel-3d :width="300" :height="300" v-if="constellationList.length !== 0" @before-slide-change="onBeforeSlideChange">
+    <button @click="goToSlide(0)">처음으로</button>
+    <carousel-3d ref="mycarousel"
+    :autoplay="true" :animationSpeed="1000" :autoplayHoverPause="true" :autoplayTimeout="3000"
+    :width="300" :height="300" :controls-visible="false" v-if="constellationList.length !== 0" @before-slide-change="onBeforeSlideChange" >
       <slide class="slide" v-for="(constellation, i) in constellationList" :key="i" :index="i">
         <span class="title text-center"> 
-        {{ constellation.contentsName }}</span>
+        {{ constellation.contentsName }}
 
         <img :src= "require('@/assets/img/constellation/' + constellation.img)" >
+
+      </span>
 
       </slide>
       </carousel-3d>
     <div v-if="constellationList.length !== 0" :constellation="constellationList[currentItem]">
-
       <div>
         {{ constellationList[currentItem].scientificName }}
       </div>
@@ -38,7 +42,6 @@
 import { listConstellation } from "@/api/constellation";
 import {Carousel3d,Slide} from "vue-carousel-3d";
 
-
 export default {
   name: 'ConstellationList',
   components: {
@@ -47,6 +50,7 @@ export default {
   },
   data() {
     return {
+      key : 0,
       currentItem: 0,    
       constellationList: [],
       constellation : null,
@@ -63,11 +67,47 @@ export default {
       }
     );
   },
+  mounted() {
+    window.addEventListener('wheel', this.handleScroll);
+    window.addEventListener('keydown', this.handleKeyDown);
+  },
+  beforeUnmount() {
+    window.removeEventListener('wheel', this.handleScroll);
+    window.removeEventListener('keydown', this.handleKeyDown);
+  },
   methods: {
     onBeforeSlideChange(index) {
       this.currentItem = index;
     },
-  },
+    goToSlide(index) {
+      this.$refs.mycarousel.goSlide(index)
+    },
+    handleScroll(event){
+      let len = this.constellationList.length;
+      if (event.deltaY > 0) {
+        this.currentItem = (len+this.currentItem-1)%len
+      }
+      else if(event.deltaY < 0){
+        this.currentItem = (len+this.currentItem+1)%len
+
+      }
+      this.$nextTick(() => {
+        this.$refs.mycarousel.goSlide(this.currentItem);
+      });
+    },
+    handleKeyDown(event) {
+      let len = this.constellationList.length;
+      if (event.key === 'ArrowRight') {
+          this.currentItem = (len+this.currentItem+1)%len
+      } else if (event.key === 'ArrowLeft') {
+          this.currentItem = (len+this.currentItem-1)%len
+      }
+      this.$nextTick(() => {
+        this.$refs.mycarousel.goSlide(this.currentItem);
+      });
+  
+    }
+  }
 };
 </script>
 
